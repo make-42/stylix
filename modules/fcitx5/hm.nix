47e5a0002b900d2fb12e1239469d20a3743a2eb4
Louis Dalibard <ontake@ontake.dev>
2025-01-01 18:57:37 +0100

    merge

--- modules/fcitx5/hm.nix
+++ modules/fcitx5/hm.nix
@@ -2,9 +2,13 @@
   config,
   lib,
   ...
+<<<<<<< ours
 }:
 let
   # Adapted from https://github.com/sanweiya/fcitx5-mellow-themes under the BSD 2 license (compatible with this project's license (MIT))
+=======
+}: let
+>>>>>>> theirs
   theme = config.lib.stylix.colors {
     template = ./theme.conf.mustache;
     extension = "conf";
@@ -17,6 +21,7 @@ let
     template = ./panel.svg.mustache;
     extension = "svg";
   };
+<<<<<<< ours
 in
 {
   options.stylix.targets.fcitx5.enable =
@@ -31,4 +36,23 @@ in
           "fcitx5/themes/stylix/theme.conf".source = theme;
         };
       };
+=======
+in {
+  options.stylix.targets.fcitx5.enable =
+    config.lib.stylix.mkEnableTarget
+    "fcitx5"
+    true;
+
+  config =
+    lib.mkIf (
+      config.stylix.enable
+      && config.stylix.targets.fcitx5.enable
+      #&& config.i18n.inputMethod.enable
+      #&& (config.i18n.inputMethod.type == "fcitx5")
+    ) {
+      xdg.dataFile."fcitx5/themes/stylix/theme.conf".source = theme;
+      xdg.dataFile."fcitx5/themes/stylix/panel.svg".source = panel;
+      xdg.dataFile."fcitx5/themes/stylix/highlight.svg".source = highlight;
+    };
+>>>>>>> theirs
 }
