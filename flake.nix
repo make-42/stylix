3a3b9c62ed7f7388701074dcf28108cd609c4c6f
Louis Dalibard <ontake@ontake.dev>
2025-03-09 21:41:02 +0100

    feat: format

--- flake.nix
+++ flake.nix
@@ -209,7 +209,7 @@
     )
     // {
       nixosModules.stylix =
-        { _pkgs ? pkgs, ... }@args:
+        { pkgs, ... }@args:
         {
           imports = [
             (import ./stylix/nixos inputs)
@@ -223,7 +223,7 @@
         };
 
       homeManagerModules.stylix =
-        { _pkgs ? pkgs, ... }@args:
+        { pkgs, ... }@args:
         {
           imports = [
             (import ./stylix/hm inputs)
@@ -236,7 +236,7 @@
         };
 
       darwinModules.stylix =
-        { _pkgs ? pkgs, ... }@args:
+        { pkgs, ... }@args:
         {
           imports = [
             (import ./stylix/darwin inputs)
