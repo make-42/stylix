60d500c13d72c47c009bf72dc92312790e54761f
Louis Dalibard <ontake@ontake.dev>
2025-03-20 18:26:59 +0100

    fix: error message to single line

--- stylix/palette.nix
+++ stylix/palette.nix
@@ -30,15 +30,9 @@ let
         in
         if diff >= 0.5 then floored + 1 else floored;
     in
-    toString (
-      lib.strings.fixedWidthString 2 "0" (lib.toHexString (round v1adj))
-    )
-    + toString (
-      lib.strings.fixedWidthString 2 "0" (lib.toHexString (round v2adj))
-    )
-    + toString (
-      lib.strings.fixedWidthString 2 "0" (lib.toHexString (round v3adj))
-    );
+    toString (lib.strings.fixedWidthString 2 "0" (lib.toHexString (round v1adj)))
+    + toString (lib.strings.fixedWidthString 2 "0" (lib.toHexString (round v2adj)))
+    + toString (lib.strings.fixedWidthString 2 "0" (lib.toHexString (round v3adj)));
 in
 {
   imports = [
@@ -47,7 +41,7 @@ in
         "stylix"
         "polarity"
       ]
-      "The stylix.polarity option has been removed as a new palette generation backend is now used.\nNew options for this palette generation backend are available at stylix.themeGeneration.\nThe new stylix.themeGeneration.polarity sets the generated theme polarity (\"dark\", \"light\" options) and the \"either\" option doesn't exist anymore.\nYou have to explicitly choose what polarity you want.\nThe new options, stylix.themeGeneration.scheme, stylix.themeGeneration.contrast, stylix.themeGeneration.primaryScale.light, and stylix.themeGeneration.primaryScale.dark, let you tweak the generated color scheme further if need be."
+      "The stylix.polarity option has been removed as a new palette generation backend is now used. New options for this palette generation backend are available at stylix.themeGeneration. The new stylix.themeGeneration.polarity sets the generated theme polarity (\"dark\", \"light\" options) and the \"either\" option doesn't exist anymore. You have to explicitly choose what polarity you want. The new options, stylix.themeGeneration.scheme, stylix.themeGeneration.contrast, stylix.themeGeneration.primaryScale.light, and stylix.themeGeneration.primaryScale.dark, let you tweak the generated color scheme further if need be."
     )
   ];
 
